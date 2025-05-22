d149a38b41c63271fdb2b20c7c760ac44a129a34
Stela David Steiner <steladsteiner@gmail.com>
2025-05-22 19:18:24 -0300

    Atualizações

--- forms/formulario.js
+++ forms/formulario.js
@@ -10,8 +10,13 @@ class Contato {
   }
 
   // Métodos
-  Enviar() {
+  Enviar(event) {
+    event.preventedDefault;
+
     let alerta = '';
+
+    console.log('Enviar: Preenchendo atributos');
+
     //if (nome esta vazio) mandar uma mensagem de erro:
     this.nome = document.getElementById('nome').value;
     if (this.nome === ''){
@@ -38,38 +43,72 @@ class Contato {
       alerta += ', data_cadastro';
     }
 
-    this.tipo_contato = document.getElementById('tipo_contato').value;
-    if (this.tipo_contato === ''){
-      alerta += ', tipo_contato';
+    // Captura o tipo de contato selecionado
+    let radios = document.getElementsByName('tipo_contato');
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            this.tipo_contato = radios[i].value;
+            break;
+        }
     }
 
+    if (this.tipo_contato === '')
+      this.tipo_contato = 'Indefinido';
+    
     if (alerta != '') {
       alert('Os seguintes campos estão com problema: ' +alerta);
     }
 
+    console.log('Enviar: Preenchendo o grid');
+
     let tabela = document.getElementById('dados');
     let linha = tabela.insertRow();
-    linha.insertCell();
-    linha.innerHTML = this.nome;
-    linha.insertCell();
-    linha.innerHTML = this.email;
-    linha.insertCell();
-    linha.innerHTML = this.fone;
-    linha.insertCell();
-    linha.innerHTML = this.mensagem;
-    linha.insertCell();
-    linha.innerHTML = this.tipo_contato;    
 
+    let col1 = linha.insertCell(0);
+    col1.textContent = this.nome;
 
+    let col2 = linha.insertCell(1);
+    col2.textContent = this.email;
 
+    let col3 = linha.insertCell(2);
+    col3.textContent = this.fone;
 
+    let col4 = linha.insertCell(3);
+    col4.textContent = this.mensagem;
+
+    let col5 = linha.insertCell(4);
+    col5.textContent = this.data_cadastro;
+
+    let col6 = linha.insertCell(5);
+    col6.textContent = this.tipo_contato;
+
+    let col7 = linha.insertCell(6);
+    col7.innerHTML = `
+            <button onClick="contato.Editar()">Editar</button>
+            <button onClick="contato.Deletar()">Deletar</button>
+        `;
+  }
+
+  // Editar
+  Editar(){
+    console.log('Editando vamos construir');
+    alert('Editando ainda sera criado');
+  }
+
+  // Deletar
+  Deletar(){
+    alert('Deletando ainda sera criado');
   }
 }
-// Instanciar a classe
-contato = new Contato();
 
-//function submeter(){
-//    var alerta = document.getElementById('nome').value;
-//    alert('Aviso: '+ alerta);
-//}
+// Instanciar a classe
+let contato = new Contato(); 
+
+/*
+function submeter(){
+  var alerta = document.getElementById('nome').value;
+  alert('Aviso: '+ alerta);
+}
+*/
+
 
